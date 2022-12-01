198b435efbe227d40cac2bc48f80bd027d68df93
Scott <pietss@farmingdale.edu>
2022-12-01 01:15:04 -0500

    alex changed some updates on the games

--- games.php
+++ games.php
@@ -59,31 +59,31 @@ echo"<table>
     
 	<td><a href='flappybird.php' >	<img src = 'Games/icons/david_bird.jpg'  alt='Play flappy bird' style='width:200;height:200;border-radius: 4px;'>		</a></td>		
 	
-    <td> <a href='snake.php'>			<img src = 'Games/icons/david_snake.jpg' 	style='width:200;height:200';>		</a></td>
+    <td> <a href='snake.php'>			<img src = 'Games/icons/david_snake.jpg' 	style='width:200;height:200;border-radius: 4px;'>		</a></td>
 	
-	<td ><a href='2048.php'>			<img src = 'Games/icons/david_2048.jpg' 	style='width:200;height:200';>			</a></td>
-	
-</tr>
-
-
-<tr>
-
-	<td ><a href='Racing.php'>	<img src = 'Games/icons/david_race.jpg' 	style='width:200;height:200';>			</a></td>
-
-    <td ><a href='DinoRun2.php'>		<img src = 'Games/icons/david_run.jpg' 		style='width:200;height:200';>			</a></td>
-
-   	<td><a href='GravityBall.php'>	<img src = 'Games/icons/david_ball.jpg' 	style='width:200;height:200';>			</a></td>
+	<td ><a href='2048.php'>			<img src = 'Games/icons/david_2048.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
 	
 </tr>
 
 
 <tr>
 
-	<td><a href='Tetris.php'>		<img src = 'Games/icons/david_tetris.jpg' 	style='width:200;height:200';>			</a></td>
+	<td ><a href='Racing.php'>	<img src = 'Games/icons/david_race.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
+
+    <td ><a href='DinoRun2.php'>		<img src = 'Games/icons/david_run.jpg' 		style='width:200;height:200;border-radius: 4px;'>			</a></td>
+
+   	<td><a href='GravityBall.php'>	<img src = 'Games/icons/david_ball.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
 	
-	<td><a href='Simon.php'>		<img src = 'Games/icons/david_simon.jpg' 	style='width:200;height:200';>			</a></td>
+</tr>
+
+
+<tr>
+
+	<td><a href='Tetris.php'>		<img src = 'Games/icons/david_tetris.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
 	
-	<td><a href='whackamole.php'>		<img src = 'Games/icons/david_mole.jpg' 	style='width:200;height:200';> 			</a></td>
+	<td><a href='Simon.php'>		<img src = 'Games/icons/david_simon.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
+	
+	<td><a href='whackamole.php'>		<img src = 'Games/icons/david_mole.jpg' 	style='width:200;height:200;border-radius: 4px;'> 			</a></td>
 	
 </tr>
 </table>";
@@ -106,11 +106,11 @@ echo"<br><table >
 
 <tr> 
 
-	<td><a href='pingpong.php'>		<img src = 'Games/icons/david_pingpong.jpg' 	style='width:200;height:200';>			</a></td>
+	<td><a href='pingpong.php'>		<img src = 'Games/icons/david_pingpong.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
 
-	<td><a href='ConnectFour.php'>		<img src = 'Games/icons/david_matchfour.jpg' 	style='width:200;height:200';>			</a></td>
+	<td><a href='ConnectFour.php'>		<img src = 'Games/icons/david_matchfour.jpg' 	style='width:200;height:200;border-radius: 4px;'>			</a></td>
 
-	<td><a href='tictactoe.php'>		<img src = 'Games/icons/david_tictactoe.jpg' 	style='width:200;height:200';> 			</a></td>
+	<td><a href='tictactoe.php'>		<img src = 'Games/icons/david_tictactoe.jpg' 	style='width:200;height:200;border-radius: 4px;'> 			</a></td>
 	
 </tr></table>";
 
